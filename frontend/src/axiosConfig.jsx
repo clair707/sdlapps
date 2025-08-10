2b1d771342f12126f246b76ea0d7bbe3e2427e65
Clair <clairxzz@hotmail.com>
2025-08-10 14:22:10 +1000

    Fix duplicate /api in API requests

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-baseURL: '/api', // local
+baseURL: '/', // local
   //baseURL: 'http://3.26.96.188:5001', // live
 headers: { 'Content-Type': 'application/json' },
 });
