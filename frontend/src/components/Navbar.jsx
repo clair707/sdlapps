2c57352229237fa7ac40620d5050a197e3e07a15
Clair <clairxzz@hotmail.com>
2025-08-10 13:21:39 +1000

    Updated NavBar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 const Navbar = () => {
