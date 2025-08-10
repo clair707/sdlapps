384ff8cfa452aca6b05cd48f6f03a1607992ee9e
Clair <clairxzz@hotmail.com>
2025-08-10 17:48:54 +1000

    Add Pet Management CRUD frontend

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,13 @@ const connectDB = require('./config/db');
 
 dotenv.config();
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/pets', require('./routes/petRoutes'));
+app.use('/api/tasks', require('./routes/taskRoutes'));
 
 // Export the app object for testing
 if (require.main === module) {
