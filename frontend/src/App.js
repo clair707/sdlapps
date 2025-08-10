6c5853a755b031178e2c79aa31d9839fd758813c
Clair <clairxzz@hotmail.com>
2025-08-10 18:38:34 +1000

    feat(appointments): CRUD UI + pet dropdown + JWT-aware API client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,9 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Tasks from './pages/Tasks';
-importgit checkout feature/pet-crud
+import Pets from "./pages/Pets";
+import Appointments from './pages/Appointments';  
+
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/tasks" element={<Tasks />} />
+        <Route path="/pets" element={<Pets />} />
+        <Route path="/appointments" element={<Appointments />} />
+
       </Routes>
     </Router>
   );
