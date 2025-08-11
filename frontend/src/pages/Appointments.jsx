77557c36deb663df6648549feebfe4f8edf03b76
Clair <clairxzz@hotmail.com>
2025-08-11 20:42:29 +1000

    PCM-15 <message>

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -12,7 +12,7 @@ const Appointments = () => {
   });
   const [editingId, setEditingId] = useState(null);
 
-  // Fetch Appointments from backend
+  // Fetch Appointments From backend
   useEffect(() => {
     fetchAppointments();
   }, []);
