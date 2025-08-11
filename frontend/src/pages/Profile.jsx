3f431892a5c18009dc1e42a23e5b3b676dd78bdb
Clair <clairxzz@hotmail.com>
2025-08-11 20:49:20 +1000

    PCM-23 <message>

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { useAuth } from '../context/AuthContext';
 import axiosInstance from '../axiosConfig';
 
 const Profile = () => {
-  const { user } = useAuth(); // Access User Token from context
+  const { user } = useAuth(); // Access User Token From context
   const [formData, setFormData] = useState({
     name: '',
     email: '',
